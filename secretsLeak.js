97d727cd720d03bf77266b6a39dc736d5ac49e93
Lukáš Cimpl <lukas.cimpl@actumdigital.com>
2025-01-30 16:08:21 +0100

    Update secretsLeak.js

--- secretsLeak.js
+++ secretsLeak.js
@@ -1,1 +1,3 @@
-// xoxb-123456789012-987654321098-abcdefghijklmnopqrstuvwx
+curl -X POST "https://io.adafruit.com/api/v2/{username}/feeds/{feed_key}/data" \
+  -H "X-AIO-Key: 1234567890abcdef1234567890abcdef" \
+  -d '{"value": "25"}'
